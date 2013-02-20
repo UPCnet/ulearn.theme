25297c003ed034693b6298b886a4f4382c3de5c4
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-02-20 17:59:06 +0100

    Override default homepage

---
+++ ulearn/theme/browser/views.py
@@ -0,0 +1,16 @@
+from five import grok
+
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from genweb.theme.browser.views import HomePageBase
+from genweb.theme.browser.interfaces import IHomePageView
+
+from ulearn.theme.browser.interfaces import IUlearnTheme
+
+
+class homePage(HomePageBase):
+    """ Override the original homepage as it will be always restricted to auth users """
+    grok.implements(IHomePageView)
+    grok.context(IPloneSiteRoot)
+    grok.require('genweb.authenticated')
+    grok.layer(IUlearnTheme)
