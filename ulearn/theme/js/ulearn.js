aef3a6a9aed03f278f85c01fedd4db8e0e2c19ae
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-07-08 14:03:42 +0200

    VArious fixes

--- ulearn/theme/js/ulearn.js
+++ ulearn/theme/js/ulearn.js
@@ -21,6 +21,7 @@ $(document).ready(function (event) {
         remote = $(targetid).data('remote');
         if (remote) {
             $(targetid).load(portal_url + "/" + remote, function (event) {
+                // OJO! Aquest codi esta duplicat per a les vistes amb i sense AJAX
                 // Favorites
                 $('#communitylist').on('click', '.favorite', function(event) {
                   event.preventDefault();
@@ -33,6 +34,67 @@ $(document).ready(function (event) {
                   }
                 });
                 $('.sortablelist').mixitup({layoutMode: 'list'});
+
+                // Dialog search communities
+                $('#communitylist').on('click', '.delete', function(event) {
+                    event.preventDefault();
+                    var $this = $(this);
+                    alertify.confirm("Si cliqueu aquí, esborrareu la comunitat " + $this.data()['name'], function (e) {
+                        if (e) {
+                            // user clicked "ok"
+                            url = $this.attr('href');
+                            $.ajax({
+                                type: "POST",
+                                url: url,
+                                data: {
+                                'form.submitted': '1',
+                                '_authenticator': $this.data()['authenticator']
+                                },
+                                error: function() {
+                                    alertify.error("Error when removing community");
+                                },
+                                success: function() {
+                                    $this.parent().parent().parent().remove();
+                                    alertify.success("Successfully removed");
+                                    console.log("ok");
+                                }
+                            });
+                        } else {
+                            // user clicked "cancel"
+                        }
+                    });
+                });
+
+                $('#communitylist').on('click', '.subscribe', function(event) {
+                    event.preventDefault();
+                    var $this = $(this);
+                    alertify.confirm("Voleu subscrivir-vos a la comunitat " + $this.data()['name'] + "?", function (e) {
+                        if (e) {
+                            // user clicked "ok"
+                            community_url = $this.data()['community'];
+                            $.ajax({
+                                type: "GET",
+                                url: community_url + "/toggle-subscribe",
+                                error: function() {
+                                    alertify.error("Error when (un)subscribing to the community");
+                                },
+                                success: function() {
+                                    if ($('i', $this).hasClass('fa-icon-check')) {
+                                        $('i', $this).addClass('fa-icon-check-empty').removeClass('fa-icon-check');
+                                        alertify.success("Successfully unsubscribed");
+                                    } else {
+                                        $('i', $this).addClass('fa-icon-check').removeClass('fa-icon-check-empty');
+                                        alertify.success("Successfully subscribed");
+                                    }
+                                    console.log("ok");
+                                }
+                            });
+                        } else {
+                            // user clicked "cancel"
+                        }
+                    });
+                });
+
             });
         }
         $('#menusup .active').removeClass('active');
@@ -63,18 +125,6 @@ $(document).ready(function (event) {
 
     });
 
-    // Favorites for search communities form
-    $('#communitylist').on('click', '.favorite', function(event) {
-      event.preventDefault();
-      var community_url = $(this).data()['community'];
-      $.get(community_url + '/toggle-favorite');
-      if ($('i', this).hasClass('fa-icon-star')) {
-        $('i', this).addClass('fa-icon-star-empty').removeClass('fa-icon-star');
-      } else {
-        $('i', this).addClass('fa-icon-star').removeClass('fa-icon-star-empty');
-      }
-    });
-
     // Community search
     $('.searchInput').on('keyup', function(event) {
         var query = $(this).val();
@@ -90,8 +140,22 @@ $(document).ready(function (event) {
         $('.currentactivity').text(int_activities + 1);
     });
 
+    // OJO! Aquest codi esta duplicat per a les vistes amb i sense AJAX
+    // Favorites for search communities form
+    $('#communitylist').on('click', '.favorite', function(event) {
+      event.preventDefault();
+      var community_url = $(this).data()['community'];
+      $.get(community_url + '/toggle-favorite');
+      if ($('i', this).hasClass('fa-icon-star')) {
+        $('i', this).addClass('fa-icon-star-empty').removeClass('fa-icon-star');
+      } else {
+        $('i', this).addClass('fa-icon-star').removeClass('fa-icon-star-empty');
+      }
+    });
+
     // Dialog search communities
     $('#communitylist').on('click', '.delete', function(event) {
+        console.log("asdadasd");
         event.preventDefault();
         var $this = $(this);
         alertify.confirm("Si cliqueu aquí, esborrareu la comunitat " + $this.data()['name'], function (e) {
