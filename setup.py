fe36b0e56649bcd469b9175f5d852c90797f8534
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-04-01 10:51:54 +0200

    Back to development: 1.48

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.47'
+version = '1.48.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
