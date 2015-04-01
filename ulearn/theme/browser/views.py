29bff618cfc2f45488ee679739917e2228c5bfb0
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-04-01 19:42:55 +0200

    New views for all communities

--- ulearn/theme/browser/views.py
+++ ulearn/theme/browser/views.py
@@ -16,6 +16,8 @@ from plone.app.users.browser.personalpreferences import UserDataPanel
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+from souper.soup import get_soup
+from repoze.catalog.query import Eq
 
 from genweb.theme.browser.views import HomePageBase
 from genweb.theme.browser.interfaces import IHomePageView
@@ -47,104 +49,24 @@ class baseCommunities(grok.View):
     grok.baseclass()
 
     def update(self):
-        self.favorites = self.get_favorites()
-        self.query = self.request.form.get('q', '')
+        self.username = api.user.get_current().id
         self.portal_url = api.portal.get().absolute_url()
 
     @memoize_contextless
     def portal(self):
         return getSite()
 
+    def get_all_communities(self):
+        pc = api.portal.get_tool('portal_catalog')
+        r_results = pc.searchResults(portal_type="ulearn.community", community_type=[u"Closed", u"Organizative"])
+        ur_results = pc.unrestrictedSearchResults(portal_type="ulearn.community", community_type=u"Open")
+        return r_results + ur_results
+
     def get_authenticator(self):
         return createToken()
 
-    def get_my_communities(self):
-        pm = api.portal.get_tool('portal_membership')
-        pc = api.portal.get_tool('portal_catalog')
-        current_user = pm.getAuthenticatedMember().getUserName()
-        return pc.searchResults(portal_type="ulearn.community",
-                                subscribed_users=current_user)
-                                #sort_on="sortable_title",)
 
-    def get_batched_communities(self, query=None, batch=True, b_size=10, b_start=0):
-        pc = getToolByName(self.context, "portal_catalog")
-        r_results = pc.searchResults(portal_type="ulearn.community", community_type=[u"Closed", u"Organizative"])
-        ur_results = pc.unrestrictedSearchResults(portal_type="ulearn.community", community_type=u"Open")
-        batch = Batch(r_results + ur_results, b_size, b_start)
-        return batch
-
-    def is_community_manager(self, community):
-        roles = api.user.get_roles(obj=community.getObject())
-
-        return 'Manager' in roles or \
-               'Site Administrator' in roles or \
-               'Owner' in roles
-
-    def get_favorites(self):
-        pm = getToolByName(self.context, "portal_membership")
-        pc = getToolByName(self.context, "portal_catalog")
-        current_user = pm.getAuthenticatedMember().getUserName()
-
-        results = pc.unrestrictedSearchResults(favoritedBy=current_user)
-        return [favorites.id for favorites in results]
-
-    def is_not_organizative(self, community):
-        return not community.community_type == u'Organizative'
-
-    def get_star_class(self, community):
-        return community.id in self.favorites and 'fa fa-star' or 'fa fa-star-o'
-
-    def authenticated_user_is_subscribed(self, community):
-        pm = getToolByName(self.context, "portal_membership")
-        user = pm.getAuthenticatedMember()
-        current_user = user.getUserName()
-        return current_user in community.subscribed_users
-
-    def favorite_button_enabled(self, community):
-        return self.authenticated_user_is_subscribed(community)
-
-    def get_subscribed_class(self, community):
-        if community.community_type == u'Organizative':
-            return ''
-        else:
-            # It's an open community or a closed one where I'm subscribed. If
-            # the community is closed and I'm not subscribed, then I should not
-            # see it as I should not have permission
-            return 'fa fa-check-square-o' if self.authenticated_user_is_subscribed(community) else 'fa fa-square-o'
-
-    def get_communities_by_query(self):
-        pc = getToolByName(self.context, "portal_catalog")
-
-        def quotestring(s):
-            return '"%s"' % s
-
-        def quote_bad_chars(s):
-            bad_chars = ["(", ")"]
-            for char in bad_chars:
-                s = s.replace(char, quotestring(char))
-            return s
-
-        if not self.query == '':
-            multispace = u'\u3000'.encode('utf-8')
-            for char in ('?', '-', '+', '*', multispace):
-                self.query = self.query.replace(char, ' ')
-
-            query = self.query.split()
-            query = " AND ".join(query)
-            query = quote_bad_chars(query) + '*'
-
-            r_results = pc.searchResults(portal_type="ulearn.community",
-                                       community_type=[u"Closed", u"Organizative"],
-                                       SearchableText=query)
-            ur_results = pc.unrestrictedSearchResults(portal_type="ulearn.community",
-                                                      community_type=u"Open",
-                                                      SearchableText=query)
-            return r_results + ur_results
-        else:
-            return self.get_batched_communities(query=None, batch=True, b_size=10, b_start=0)
-
-
-class communities(baseCommunities):
+class AllCommunities(baseCommunities):
     """ The list of communities """
     grok.context(IPloneSiteRoot)
     grok.require('genweb.member')
@@ -157,15 +79,6 @@ class communitiesAJAX(baseCommunities):
     grok.context(IPloneSiteRoot)
     grok.require('genweb.member')
     grok.template('my_communities_ajax')
-    grok.layer(IUlearnTheme)
-
-
-class searchCommunitiesAJAX(baseCommunities):
-    """ Search communities via AJAX """
-    grok.name('search-communities-ajax')
-    grok.context(IPloneSiteRoot)
-    grok.require('genweb.member')
-    grok.template('search_communities_ajax')
     grok.layer(IUlearnTheme)
 
 
--- ulearn/theme/portlets/homebuttonbar.py
+++ ulearn/theme/portlets/homebuttonbar.py
@@ -1,5 +1,5 @@
 from zope.interface import implements
-
+from plone import api
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
 
@@ -22,6 +22,10 @@ class Assignment(base.Assignment):
 class Renderer(base.Renderer):
 
     render = ViewPageTemplateFile('templates/buttonbar.pt')
+
+    def __init__(self, context, request, view, manager, data):
+        super(Renderer, self).__init__(context, request, view, manager, data)
+        self.portal_url = api.portal.get().absolute_url()
 
 
 class AddForm(base.NullAddForm):
