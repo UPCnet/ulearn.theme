b07a37bb897f0dd9de58a50c33263aa6f35cc6cf
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-05-06 17:41:42 +0200

    Updated for complete profile generic enough for not to override it

--- ulearn/theme/browser/user_profile.py
+++ ulearn/theme/browser/user_profile.py
@@ -5,6 +5,7 @@ from zope.interface import implements
 from zope.component import getUtility
 from zope.component.hooks import getSite
 from zope.publisher.interfaces import IPublishTraverse, NotFound
+from zope.component import getUtilitiesFor
 
 from plone.memoize.view import memoize_contextless
 from plone.registry.interfaces import IRegistry
@@ -13,56 +14,93 @@ from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from souper.interfaces import ICatalogFactory
+
 from mrs.max.utilities import IMAXClient
 from ulearn.core.badges import AVAILABLE_BADGES
 from ulearn.core.controlpanel import IUlearnControlPanelSettings
+from genweb.core.utils import get_safe_member_by_id
+from ulearn.core import _
+
+from plone import api
 
 
 class userProfile(BrowserView):
     """ Return an user profile ../profile/{username} """
     implements(IPublishTraverse)
 
+    index = ViewPageTemplateFile('views_templates/user_profile.pt')
+
     def __init__(self, context, request):
         super(userProfile, self).__init__(context, request)
         self.username = None
-
-    def publishTraverse(self, request, name):
-        if self.username is None:  # ../profile/username
-            self.username = name
-        else:
-            raise NotFound(self, name, request)
-        return self
-
-    index = ViewPageTemplateFile('views_templates/user_profile.pt')
+        self.portal = api.portal.get()
+        self.portal_url = self.portal.absolute_url()
 
     def __call__(self):
         return self.index()
 
-    @memoize_contextless
-    def portal_url(self):
-        return self.portal().absolute_url()
+    def publishTraverse(self, request, name):
+        if self.username is None:  # ../profile/username
+            self.username = name
+            self.user_info = get_safe_member_by_id(self.username)
+        else:
+            raise NotFound(self, name, request)
+        return self
 
-    @memoize_contextless
-    def portal(self):
-        return getSite()
+    def get_posts_literal(self):
+        literal = api.portal.get_registry_record(name='ulearn.core.controlpanel.IUlearnControlPanelSettings.people_literal')
+        if literal == 'thinnkers':
+            return 'thinnkins'
+        else:
+            return 'entrades'
 
     def has_complete_profile(self):
-        pm = getToolByName(self.portal(), 'portal_membership')
-        user = pm.getAuthenticatedMember()
-        portrait = pm.getPersonalPortrait()
+        if self.user_info:
+            pm = api.portal.get_tool('portal_membership')
+            portrait = pm.getPersonalPortrait()
 
-        if user.getProperty('fullname') \
-           and user.getProperty('fullname') != user.getProperty('username') \
-           and user.getProperty('email') \
-           and isinstance(portrait, Image):
-            return True
+            if self.user_info.get('fullname', False) \
+               and self.user_info.get('fullname', False) != self.username \
+               and self.user_info.get('email', False) \
+               and isinstance(portrait, Image):
+                return True
+            else:
+                return False
         else:
+            # The user doesn't have any property information for some weird
+            # reason or simply beccause we are admin
             return False
 
+    def get_user_info_for_display(self):
+        user_properties_utility = getUtility(ICatalogFactory, name='user_properties')
+
+        try:
+            client = api.portal.get_registry_record('mrs.max.browser.controlpanel.IMAXUISettings.domain')
+        except:
+            client = ''
+
+        rendered_properties = []
+        if 'user_properties_{}'.format(client) in [a[0] for a in getUtilitiesFor(ICatalogFactory)]:
+            extended_user_properties_utility = getUtility(ICatalogFactory, name='user_properties_{}'.format(client))
+            for prop in extended_user_properties_utility.profile_properties:
+                rendered_properties.append(dict(
+                    name=_(prop),
+                    value=self.user_info.get(prop, '')
+                ))
+            return rendered_properties
+        else:
+            # If it's not extended, then return the simple set of data we know
+            # about the user using also the profile_properties field
+            for prop in user_properties_utility.profile_properties:
+                rendered_properties.append(dict(
+                    name=_(prop),
+                    value=self.user_info.get(prop, '')
+                ))
+            return rendered_properties
+
     def get_member_data(self):
-        pm = getToolByName(self.portal(), 'portal_membership')
-        user = pm.getMemberById(self.username)
-        return user
+        return api.user.get_current()
 
     def user_properties(self):
         member_data = self.get_member_data()
@@ -75,36 +113,3 @@ class userProfile(BrowserView):
                 'telefon': member_data.getProperty('telefon'),
                 'ubicacio': member_data.getProperty('ubicacio'),
                 }
-
-    def get_badges(self):
-        """ Done consistent with an hipotetical badge provider backend """
-        # Call to the REST service for the user badges returning a list with the
-        # (for example 4 more recent badges or the user selected badges)
-        # >>> connect_backpack(self.username.getId(), app="ulearn", sort="user_preference", limit=4)
-        # >>> [{"displayName": "Code Whisperer", "id":"codewhisperer", "png": "http://...", "icon": "trophy"}, ]
-
-        badges = deepcopy(AVAILABLE_BADGES)
-        if self.has_complete_profile():
-            badges[0]['awarded'] = True
-
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(IUlearnControlPanelSettings, check=False)
-
-        thinnkins = self.get_thinnkins()
-        if thinnkins:
-            if (thinnkins >= int(settings.threshold_winwin1)):
-                badges[1]['awarded'] = True
-            if (thinnkins >= int(settings.threshold_winwin2)):
-                badges[2]['awarded'] = True
-            if (thinnkins >= int(settings.threshold_winwin3)):
-                badges[3]['awarded'] = True
-
-        return badges
-
-    @memoize_contextless
-    def get_thinnkins(self):
-        maxclient, settings = getUtility(IMAXClient)()
-        maxclient.setActor(settings.max_restricted_username)
-        maxclient.setToken(settings.max_restricted_token)
-
-        return maxclient.people[self.username].activities.head()
