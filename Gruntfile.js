d4f7d2b54371511dfc3f9e427e27fce592407497
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-09-07 14:41:09 +0200

    Not clean resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -198,7 +198,7 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('gwbuild', [
-    'clean:dist',
+    // 'clean:dist',
     'cssmin:dist',
     'filerev:build',
     'updateconfig'
