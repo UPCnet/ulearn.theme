f9b75634605f14d631f5ed42310f76bc60dbb3a2
Pilar Marinas <pilar.marinas@upcnet.es>
2016-12-05 08:54:10 +0100

    Back to development: 2.46

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.45'
+version = '2.46.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
